4732c4ec1506cab274173d2b285707f78b7ff3ce
Felipe Lima <felipe@ferreiradelima.com>
2020-07-24 00:08:19 -0300

    fix ts builder and added banner on run

---
+++ externals.js
@@ -0,0 +1,24 @@
+const requireExternal = (module) => `require("${module}")`;
+
+function externals(force = false) {
+  if (force) return {};
+
+  return {
+    'tago/analysis': requireExternal('tago/analysis'),
+    'tago/device': requireExternal('tago/device'),
+    'tago/account': requireExternal('tago/account'),
+    'tago/services': requireExternal('tago/services'),
+    'tago/utils': requireExternal('tago/utils'),
+    'moment-timezone': requireExternal('moment-timezone'),
+    '@tago-io/sdk': requireExternal('@tago-io/sdk'),
+    tago: requireExternal('tago'),
+    async: requireExternal('async'),
+    moment: requireExternal('moment'),
+    co: requireExternal('co'),
+    lodash: requireExternal('lodash'),
+    underscore: requireExternal('lodash'),
+    _: requireExternal('lodash'),
+  };
+}
+
+module.exports = externals;
