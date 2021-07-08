f8a280a19f2e1c577cf9bbed39a5e1046bdc65fd
Felipe Lima <felipe@ferreiradelima.com>
2021-07-08 18:33:42 -0300

    migrate from webpack to esbuild

--- externals.js
+++ externals.js
@@ -1,27 +1,25 @@
-const requireExternal = (module) => `require("${module}")`;
-
 function externals(force = false) {
-  if (force) return {};
+  if (force) return undefined;
 
-  return {
-    'tago/analysis': requireExternal('tago/analysis'),
-    'tago/device': requireExternal('tago/device'),
-    'tago/account': requireExternal('tago/account'),
-    'tago/services': requireExternal('tago/services'),
-    'tago/utils': requireExternal('tago/utils'),
-    'moment-timezone': requireExternal('moment-timezone'),
-    '@tago-io/sdk': requireExternal('@tago-io/sdk'),
-    'date-fns': requireExternal('date-fns'),
-    'date-fns-tz': requireExternal('date-fns'),
-    'lora-packet': requireExternal('lora-packet'),
-    dayjs: requireExternal('dayjs'),
-    luxon: requireExternal('luxon'),
-    tago: requireExternal('tago'),
-    geolib: requireExternal('geolib'),
-    moment: requireExternal('moment'),
-    axios: requireExternal('axios'),
-    lodash: requireExternal('lodash'),
-  };
+  return [
+    'tago/analysis',
+    'tago/device',
+    'tago/account',
+    'tago/services',
+    'tago/utils',
+    'moment-timezone',
+    '@tago-io/sdk',
+    'date-fns',
+    'date-fns-tz',
+    'lora-packet',
+    'dayjs',
+    'luxon',
+    'tago',
+    'geolib',
+    'moment',
+    'axios',
+    'lodash',
+  ];
 }
 
 module.exports = externals;
